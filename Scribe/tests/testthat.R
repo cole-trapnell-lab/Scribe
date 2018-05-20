4769fedf3c638252d564b4dd2a68b637dae1f211
Xiaojie Qiu <xqiu@uw.edu>
2018-05-19 22:23:24 -0700

    clear up the current directory

--- Scribe/tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(InformationEstimator)
-
-test_check("InformationEstimator")
--- Scribe/tests/testthat/test.R
+++
@@ -1,8 +0,0 @@
-#test InformationEstimator
-require(Rcpp)
-
-cppFunction("
-NumericMatrix exfun(NumericMatrix x){
-  NumericMatrix::
-}
-            ")
